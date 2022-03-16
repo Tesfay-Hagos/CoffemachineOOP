9b39ea45927a68172ca8d2026b3e0ae48c76d8f4
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2022-03-16 11:35:01 +0000

    write the code for the Main class, which actually make the coffee machine

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+is_continue=True
+while(is_continue):
+  menu=Menu()
+  moneyMachine=MoneyMachine()
+  cofffeeMaker=CoffeeMaker()
+  choice=input(f"What Would you like {menu.get_items()}")
+  if(choice == 'off'):
+    is_continue = False
+  elif(choice == "report"):
+    cofffeeMaker.report()
+    moneyMachine.report()
+  elif(choice == 'latte','espresso','cappuccino'):
+    drink=menu.find_drink(choice)
+    if(cofffeeMaker.is_resource_sufficient(drink)):
+      if(moneyMachine.make_payment(drink.cost)):
+        cofffeeMaker.make_coffee(drink)
+        
+    
+  
